2c63457c8c549ec34f1da4ba48b3e93eb685936c
Nick Clarkson <orko@unknown-00-23-12-0d-e1-00.home>
2012-11-09 01:04:28 +0000

    fixed duplicate socket connections

--- www/js/socket.js
+++ www/js/socket.js
@@ -6,19 +6,23 @@ var socket = (function(){
   
   return {
     connect: function(cb){
-      ws = new WebSocket("ws://echo.websocket.org/");
-      ws.onopen = function(evt){
-        $ci.addClass('connected').find('a').click(function(e){
-          e.preventDefault();
-          socket.send($(this).attr('href'));
-        });
+      if(ws){
         cb();
-      };
-      ws.onclose = function(evt){  };
-      ws.onmessage = function(evt){ 
-        channel.publish(JSON.parse(evt.data));
-      };
-      ws.onerror = function(evt){ };
+      } else {
+        ws = new WebSocket("ws://echo.websocket.org/");
+        ws.onopen = function(evt){
+          $ci.addClass('connected').find('a').click(function(e){
+            e.preventDefault();
+            socket.send($(this).attr('href'));
+          });
+          cb();
+        };
+        ws.onclose = function(evt){  };
+        ws.onmessage = function(evt){ 
+          channel.publish(JSON.parse(evt.data));
+        };
+        ws.onerror = function(evt){ };
+      }
     },
     disconnect: function(){
       $ci.removeClass('connected').unbind();
